99c8f7a168e5e955fc9033e29235635d030c34db
liekevdvoort <lieke.voort1@gmail.com>
2022-06-09 09:10:27 +0200

    variable circles

--- custombg.js
+++ custombg.js
@@ -1,7 +1,12 @@
 class SimonBg {
+
+    static get inputProperties(){
+        return ['--number-of-circles'];
+    }
+
     paint(context, size, props){
       
-        for(var i = 0; i < 8; i++){
+        for(var i = 0; i < props.get('--number-of-circles'); i++){
             let fraction = i * 2;
 
             context.beginPath();
@@ -11,7 +16,7 @@ class SimonBg {
         }
         
         context.beginPath();
-        context.moveTo(8 * 23, size.height - 10);
+        context.moveTo(props.get('--number-of-circles') * 23, size.height - 10);
         context.lineTo(size.width, size.height - 10)
         context.stroke();
     }
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+CSS.registerProperty({
+    name: '--number-of-circles',
+    syntax: '<integer>',
+    inherits: false,
+    initialValue: '8',
+  })
+
 CSS.paintWorklet.addModule('custombg.js');
